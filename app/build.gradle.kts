dcfdecfbb37197f3389e4d64f14659979e87f17e
abidsuhail <abid1294005@gmail.com>
2026-02-01 22:03:11 +0300

    new version v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.abidsuhail"
                 artifactId = "livDebug"
-                version = "2.0.0"
+                version = "2.0.1"
             }
         }
     }
